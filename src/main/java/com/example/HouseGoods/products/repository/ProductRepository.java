b2f4c0b714f62a638851358d8756e325fdefc4be
armen <armenpetrosan319@gmail.com>
2026-04-02 10:45:20 +0500

    Определил работу репозиториев

---
+++ src/main/java/com/example/HouseGoods/products/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.HouseGoods.products.repository;
+
+import com.example.HouseGoods.products.Product;
+import com.example.HouseGoods.products.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    Optional<List<Product>> findByCategory(Category category);
+}
