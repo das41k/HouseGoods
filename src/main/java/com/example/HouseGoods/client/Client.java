011f3940fcc5356b8b75c8c296eb845e85061284
armen <armenpetrosan319@gmail.com>
2026-04-07 11:56:23 +0500

    Определил структуру клиента и написал репозиторий к нему

---
+++ src/main/java/com/example/HouseGoods/client/Client.java
@@ -0,0 +1,31 @@
+package com.example.HouseGoods.client;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "clients")
+@Data
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone", unique = true, nullable = false)
+    private String phone;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private ClientRole role;
+}
