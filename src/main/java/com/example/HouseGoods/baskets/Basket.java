b047ecf2b27437a511ffe6e8c800b552b435b04f
armen <armenpetrosan319@gmail.com>
2026-04-18 12:12:09 +0500

    Определил основные сущности для работы с корзиной

---
+++ src/main/java/com/example/HouseGoods/baskets/Basket.java
@@ -0,0 +1,27 @@
+package com.example.HouseGoods.baskets;
+
+import com.example.HouseGoods.client.Client;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "baskets")
+@Data
+public class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "basket_id")
+    private Long basketId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
