02bad378e9d5cab7289bc0a6285ae7542ffc14bd
armen <armenpetrosan319@gmail.com>
2026-04-19 16:57:18 +0500

    Реализовано создание заказа

--- src/main/java/com/example/HouseGoods/orders/controller/OrderController.java
+++ src/main/java/com/example/HouseGoods/orders/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.HouseGoods.orders.controller;
 
+import com.example.HouseGoods.orders.dto.CreateOrderRequest;
 import com.example.HouseGoods.orders.dto.OrderResponse;
 import com.example.HouseGoods.orders.dto.OrderResponseByUser;
 import com.example.HouseGoods.orders.service.OrderService;
@@ -8,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +35,14 @@ public class OrderController {
         log.debug("GET /api/orders/{}", id);
         String email = clientDetails.getUsername();
         return ResponseEntity.ok(orderService.getOrderById(id, email));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createOrder(@RequestBody CreateOrderRequest request,
+             @AuthenticationPrincipal ClientDetails clientDetails) {
+        log.debug("POST /api/orders");
+        String email = clientDetails.getUsername();
+        orderService.createOrder(request, email);
+        return ResponseEntity.ok("Заказ был успешно создан!");
     }
 }
